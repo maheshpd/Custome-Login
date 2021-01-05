2eac65b3e59ca826ae89c8e2ed8f02b41c468797
Mahesh Prasad <mahesh223prasad@gmail.com>
2021-01-05 18:23:39 +0530

    Initial Commit

---
+++ Custome Login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custome Login
+//
+//  Created by Mahesh Prasad on 05/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custome Login/Custome_LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custome_LoginApp.swift
+//  Custome Login
+//
+//  Created by Mahesh Prasad on 05/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Custome_LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
